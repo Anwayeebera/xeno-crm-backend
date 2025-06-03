4730a3aac7fb02340795272ea055852921da0ec2
Anwayeebera <146617989+Anwayeebera@users.noreply.github.com>
2025-06-03 12:13:35 +0530

    fixed dotenv config

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import 'dotenv/config';
+// import  'dotenv/config';
 import express from 'express';
 import connectDB from './config/db.js';
 import cors from 'cors';
@@ -10,7 +10,10 @@ import deliveryReceiptRoutes from './routes/deliveryReceiptRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import aiRoutes from './routes/aiRoutes.js';
 import { protect } from './middleware/authMiddleware.js';
+import config from  'dotenv/config';
 
+
+config(); // Load environment variables
 
 const app = express();
 connectDB();
@@ -48,7 +51,7 @@ app.use((err, req, res, next) => {
     res.status(500).json({ error: 'Internal server error' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
